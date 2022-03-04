64c14b33c6e20500243fa7463bf6d780032bede0
NazarovDA <d.n.0@bk.ru>
2022-03-04 23:36:24 +0300

    untested changes. added img splice and resizing

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class Rect:
         )
 
     def __repr__(self) -> str:
-        return f"lat1={self.lat1}, lat2={self.lat2}, lon1={self.lon1}, lon2={self.lon2}"
+        return f"\nlat1={self.lat1}, lat2={self.lat2}, lon1={self.lon1}, lon2={self.lon2}"
 
 class Worker:
     dataset_path = "./png_fullsize/"
@@ -83,35 +83,38 @@ class Worker:
     
     def openImage(self, filename: str) -> numpy.ndarray: return cv2.imread(filename)
 
+    def getImageSlice(self, x1, x2, y1, y2, image: numpy.ndarray) -> numpy.ndarray:
+        return image[y1:y2, x1:x2]
+
+    def getFile(self, point1: Point, point2: Point) -> numpy.ndarray:
+
+        image = self.openImage(self.getFileName(point1))
+        #print(self.toNSEWString(lat=point1.lat, lon=point1.lon))
+        nsew = self.fromNSEWString(self.toNSEWString(lat=point1.lat, lon=point1.lon))
+        height = len(image)
+        width = len(image[0])
+
+        x1 = math.floor((point1.lat - nsew.lat1) * width)
+        y1 = math.floor((point1.lon - nsew.lon1) * height)
+        x2 = math.ceil((point2.lat - nsew.lat1) * width)
+        y2 = math.ceil((point2.lon - nsew.lon1) * height)
+
+        slice = self.getImageSlice(
+                image=image,
+                x1=x1, y1=y1,
+                x2=x2, y2=y2,
+            ) 
+
+        return slice
+
+
     def getFiles(self, point1: Point, point2: Point) -> list[numpy.ndarray]:
-        """
-        
-        """
         dlat = abs(point1.lat - point2.lat)
         dlon = abs(point1.lon - point2.lon)
         number_of_chunks = (math.floor(dlat) + 1) * (math.floor(abs(dlon)) + 1)
 
         if number_of_chunks == 1:
-            image = self.openImage(self.getFileName(point1))
-            #print(self.toNSEWString(lat=point1.lat, lon=point1.lon))
-            nsew = self.fromNSEWString(self.toNSEWString(lat=point1.lat, lon=point1.lon))
-            height = len(image)
-            width = len(image[0])
-
-            x1 = math.floor((point1.lat - nsew.lat1) * width)
-            y1 = math.floor((point1.lon - nsew.lon1) * height)
-            x2 = math.ceil((point2.lat - nsew.lat1) * width)
-            y2 = math.ceil((point2.lon - nsew.lon1) * height)
-            
-            #print(x1, x2, y1, y2)
-
-            slice = self.getImageSlice(
-                image=image,
-                x1=x1, y1=y1,
-                x2=x2, y2=y2,
-            )
-
-            return slice
+            return self.getFile(point1, point2)
             # TODO
 
         elif number_of_chunks > 1:
@@ -124,26 +127,32 @@ class Worker:
             if lon2 < lon1: lon1, lon2 = lon2, lon1 # lon1 is lower lon, lon2 is higher
 
             if dlat > 1:
+                latsBetween.append(lat1)
+                latsBetween.append(lat2)
                 lat1 = math.floor(lat1)
                 lat2 = math.ceil(lat2)
-
-                latsBetween.append(lat1)
                 latsBetween += self.__searchIntsBetween(lat1, lat2)
-                latsBetween.append(lat2)
+                
             else:
                 latsBetween = [lat1, lat2]
 
             if dlon > 1:
+                lonsBetween.append(lon1)
+                lonsBetween.append(lon2)
+
                 lon1 = math.floor(lon1)
                 lon2 = math.ceil(lon2)
-
-                lonsBetween.append(lon1)
+                
                 lonsBetween += self.__searchIntsBetween(lon1, lon2)
-                lonsBetween.append(lon2)
             else: 
                 lonsBetween = [lon1, lon2]
 
+            latsBetween.sort()
+            lonsBetween.sort()
+
             rects: list[Rect] = list()
+
+            print(lonsBetween, latsBetween)
 
             for latIdx, lat in enumerate(latsBetween):
                 if latIdx == 0: continue
@@ -159,19 +168,28 @@ class Worker:
 
                     rects.append(rect)
 
-            print(rects)
-                    
+            images = [self.getFile(rect.point1, rect.point2) for rect in rects]
+            
+            rowsLen, columnsLen = latsBetween.__len__() - 1, lonsBetween.__len__() - 1
 
+            img = None
 
-            raise Exception("Unimplemented") # TODO: own exceptions
+            print(rects.__len__())
+            a = 0
+            for rowNumber in range(rowsLen):
+                image:numpy.ndarray = None
+                for columnNumber in range(columnsLen):
+                    image = images[0] if columnNumber == 0 else numpy.concatenate(image, images[a], axis=1)
+                    a+=1
+
+                img = image if rowNumber == 0 else numpy.concatenate(img, image, axis=0)
+
+            return cv2.resize(img, (2000, 2000))
+            
 
         else: raise errors.NoChunksRequired()
         
         # if number_of_chunks != 1: N images
-
-        
-    def getImageSlice(self, x1, x2, y1, y2, image: numpy.ndarray) -> numpy.ndarray:
-        return image[y1:y2, x1:x2]
 
     def __searchIntsBetween(self, lower: int, higher: int) -> list[int]:
         if lower > higher: lower, higher = higher, lower
@@ -190,7 +208,7 @@ class Worker:
 filename = "./png_fullsize/S026W049.png"
 
 wk = Worker()
-img = wk.getFiles(Point(-27.7, -48.5), Point(-25.3, -48.1))
+img = wk.getFiles(Point(-27.7, -48.5), Point(-25.3, -44.1))
 # new_img = img[0:550, 0:200] # [y1:y2, x1:x2]
 # #print(new_img)
 
